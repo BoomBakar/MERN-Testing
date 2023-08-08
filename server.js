0e6204791049d42c3d023a99985f67676ab2ed2e
Abubakar Dar <abubakar@xavor.com>
2023-08-08 10:57:22 +0500

    Latest version

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const dbConnect = require('./database/db');
 
 app.get('/', (req,res) => {
     res.send('hello world');
 })
 
-app.listen(porcess.env.PORT , () => {
+dbConnect();
+
+app.listen(process.env.PORT , () => {
     console.log(`server is running on port ${process.env.PORT}`);
 })
